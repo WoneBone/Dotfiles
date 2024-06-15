1c59155f221ca4fbb141725ed57bd3ae9fd6fc79
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-06-15 23:53:43 +0100

    COC good, lualine bad

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -41,6 +41,9 @@ vim.opt.cindent = true
 --Folds
 vim.opt.foldmethod='indent'
 
+--Remap ill forguet to use
+vim.keymap.set('n', '<Leader>n', '<cmd>cnext<cr>')
+vim.keymap.set('n', '<Leader>N', '<cmd>cNext<cr>')
 
 -- Save undo history
 vim.opt.undofile = true

d74135a30ae428b32376f46a772f7059084d46db
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-06-15 18:23:58 +0100

    Migration finished

---
+++ nvim/.config/nvim/lua/configs/lazy/treeSitter.lua
@@ -0,0 +1,17 @@
+return{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "python", "bash" },
+          sync_install = false,
+          highlight = {
+			  enable = true,
+			  additional_vim_regex_highlighting = { "markdown" },
+	  },
+          indent = { enable = true },  
+        })
+    end
+ }

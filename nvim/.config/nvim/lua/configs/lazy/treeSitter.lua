e8cbd14e74286fea2534f0fc0b6b0dc78b17d612
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.pt>
2025-01-13 16:01:11 +0000

    Working commit

--- nvim/.config/nvim/lua/configs/lazy/treeSitter.lua
+++ nvim/.config/nvim/lua/configs/lazy/treeSitter.lua
@@ -5,7 +5,7 @@ return{
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "python", "bash", "markdown", "markdown_inline", "latex" },
+          ensure_installed = { "c", "lua", "vimdoc", "python", "markdown", "markdown_inline" },
           sync_install = false,
           highlight = {
 			  enable = true,
